eaecae6506e4af1dbea9f82815c762e23d06f4cb
BrandonAbe <brandonaberosenblatt@gmail.com>
2019-03-28 10:57:23 -0400

    Added right brake code

--- Chairty.ino
+++ Chairty.ino
@@ -1,15 +1,12 @@
 
 /*
  Project Charity
-
- This program (so far) drives a  bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
- The motor is controlled based on the analog value from the capacitive touch sensor attached to Pin A0.
  
  Created 03 March 2019
  Rev0 on 03 March 2019
  Rev1 on 05 March 2019
  Rev2 on 09 March 2019 - Left Brake now works
+ Rev3 on 26 March 2019 - Both Brakes working
  by Brandon Rosenblatt
 
  */
@@ -25,16 +22,18 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(A0); // default value is 0 on startup
- 
   if (sensorValue > 600 )
   {
     Serial.println("Button is Pressed");
     Serial.println(sensorValue);
     leftServo.attach(2,750,2000);  // attaches the left servo on pin 2 to the servo object
+    rightServo.attach(3,750,2000);  // attaches the left servo on pin 2 to the servo object
    for (leftPos = 0; leftPos <= 0; leftPos -= 1) { // goes from 0 degrees to 180 degrees
     leftServo.write(10);        // tell servo to go to move 10°
+    rightServo.write(10);
     delay(5000);                       // This delay controls the time that the brakes are turned on
     leftServo.detach();
+    rightServo.detach();
     break;
   }
   }
@@ -44,5 +43,6 @@ void loop() {
     Serial.println("Button is NOT Pressed");
     Serial.println(sensorValue);
     leftServo.detach();
+    rightServo.detach();
    }
 }
