998a115cec1eaf898eb5e1e917bfbdcfe5983d4d
BrandonAbe <brandonaberosenblatt@gmail.com>
2019-04-29 21:13:54 -0400

    Update ChairityFinal.ino
    Lots of updates and deletion of irrelevant code

--- ChairityFinal.ino
+++ ChairityFinal.ino
@@ -22,15 +22,9 @@ const int rightRelay = 52; // sets control pin for left relay
 //Gyroscope Constants
 const int MPU=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-double pitch,roll,yaw;
-double pitchAvg = -0.169; //Averages across 13 samples
-double rollAvg = 0.532; //Average across 13 samples
-double xAvg = -3007; double yAvg = -249; double zAvg = -82;
+double pitch,roll,yaw; // Double float variables for pitch, roll, and yaw
 //Set gyro limits
-double pitchLimit, yawLimit, rollLimit;
-//pitchLimit = 0;
-//yawLimit = 0;
-//rollLimit = 0;
+double pitchLimit, yawLimit, rollLimit; // Variables for limiting pitch, roll, and yaw
 
 void setup() {
   // initialize the serial port:
@@ -41,11 +35,11 @@ void setup() {
   pinMode(rightRelay, OUTPUT); // Allow right relay to be an output
 
   //Gyro Code
-  Wire.begin();
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
+  Wire.begin(); 
+  Wire.beginTransmission(MPU); //Begin transmission to gyro
+  Wire.write(0x6B); //Write to this location
+  Wire.write(0);    //The value 0
+  Wire.endTransmission(true); //End transmission
 }
 
 void loop() {
@@ -53,22 +47,22 @@ void loop() {
   if (sensorValue > 600 )
   {
     Serial.println("Button is Pressed");
-    Serial.println(sensorValue);
+    //Serial.println(sensorValue);   // Uncomment to display analog value from touch sensor for debugging purposes. 
     leftServo.attach(2,750,2000);  // attaches the left servo on pin 2 to the servo object
-    rightServo.attach(3,750,2000);  // attaches the left servo on pin 2 to the servo object
+    rightServo.attach(3,750,2000);  // attaches the right servo on pin 3 to the servo object
+   
    for (leftPos = 0; leftPos <= 0; leftPos -= 1) { // goes from 0 degrees to 180 degrees
-    leftServo.write(10);        // tell servo to go to move 10°
-    rightServo.write(10);
-    actuateUp();
-    //actuate();
-    actuateDown();
-    leftServo.detach();
-    rightServo.detach();
-    break;
+    leftServo.write(10);        //  tell left servo to go to move 10°
+    rightServo.write(10);       // tell right servo to go to move 10°
+    actuateUp();                // Actuates the servo up
+    actuateDown();              // Actuates the servo back down
+    leftServo.detach();         // Turns off left servo movement
+    rightServo.detach();        // Turns off left servo movement
+    break;                      // Exit loop
   }
   }
   
-  if (sensorValue <= 600)
+  if (sensorValue <= 600) // if capacitive sensor is not pressed
   {
     Serial.println("Button is NOT Pressed");
     Serial.println(sensorValue);
@@ -76,40 +70,16 @@ void loop() {
     rightServo.detach();
    }
 }
-/*void actuate(){
- //Up for 3.5 seconds
-  digitalWrite(leftRelay, HIGH);
-  digitalWrite(rightRelay, LOW);//Activate the relay the other direction, they must be different to move the motor
-  delay(35000);// wait 35 seconds
-  digitalWrite(leftRelay, HIGH);
-  digitalWrite(rightRelay, HIGH);//Deactivate both relays to brake the motor
-  delay(500);// wait .5 seconds
- 
-  //Down for 3.5 seconds
-  digitalWrite(leftRelay, LOW);
-  digitalWrite(rightRelay, HIGH);//Activate the relay one direction, they must be different to move the motor
-  delay(35000); //wait 35 seconds
-  digitalWrite(leftRelay, HIGH);
-  digitalWrite(rightRelay, HIGH);//Deactivate both relays to brake the motor
-  delay(500);// wait .5 seconds buffer time before another commmand can occur
-}
-*/
+
 void actuateUp(){
- digitalWrite(leftRelay, HIGH);
- digitalWrite(rightRelay, LOW);//Activate the relay the other direction, they must be different to move the motor
+ digitalWrite(leftRelay, HIGH); //Control Polarity of Motor
+ digitalWrite(rightRelay, LOW); //Control Polarity of Motor
  for (int i = 0; i <= 3500; i++){
     //Gyro Code comparison
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU,14,true);
-
-    int GyXoff,GyYoff,GyZoff;
-
-    //Gyro correction
-    GyXoff = -1*xAvg;
-    GyYoff = -1*yAvg;
-    GyZoff = -1*zAvg;
 
     //read gyro data
     GyX=(Wire.read()<<8|Wire.read())/100;
@@ -118,13 +88,7 @@ void actuateUp(){
 
     //get pitch/roll
     getAngle(AcX,AcY,AcZ);
-
-    //send the data out the serial port
-    //Serial.print("Angle (in degrees): ");
-    //Serial.print("Pitch = "); Serial.print(pitch);
-    //Serial.print(" | Roll = "); Serial.println(roll);
-
-
+  
     Serial.print("Gyroscope: ");
     Serial.print("X = "); Serial.print(GyX);
     Serial.print(" | Y = "); Serial.print(GyY);
@@ -145,8 +109,8 @@ void actuateUp(){
 
  }
 
- digitalWrite(leftRelay, HIGH);
- digitalWrite(rightRelay, HIGH);//Deactivate both relays to brake the motor
+ digitalWrite(leftRelay, HIGH); //Control Polarity of Motor
+ digitalWrite(rightRelay, HIGH); //Control Polarity of Motor
  delay(5000);// wait 5 seconds
 }
 
